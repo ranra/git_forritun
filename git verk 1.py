7eb7f80156aba5e9d5d830a62e0c9e31cd809515
Arnar Orri Arnarsson <arnarar292@nemi.tskoli.is>
2017-01-25 08:52:09 +0000

    git verk 1 dæmi2

--- git verk 1.py
+++ git verk 1.py
@@ -1,8 +1,15 @@
 #dæmi1
-
+'''
 tala1=int(input("sláðu inn tölu"))
 tala2=int(input("sláðu inn aðra tölu"))
 marg=tala1*tala2
 plus=tala1+tala2
 print("tölurnar lagðar saman = "+str(plus)+" tölurnar margfaldaðar saman = "+str(marg))
 
+'''
+
+#dæmi 2
+
+fornafn=input("slaðu inn fornafn ")
+eftirnafn =input("sláðu inn eftirnafn ")
+print("halló "+fornafn,eftirnafn)
