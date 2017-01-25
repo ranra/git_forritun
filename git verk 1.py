ab65b8b03864abedaa30f73deb388561d2f082c7
Arnar Orri Arnarsson <arnarar292@nemi.tskoli.is>
2017-01-25 08:47:35 +0000

    git verk 1

---
+++ git verk 1.py
@@ -0,0 +1,8 @@
+#dæmi1
+
+tala1=int(input("sláðu inn tölu"))
+tala2=int(input("sláðu inn aðra tölu"))
+marg=tala1*tala2
+plus=tala1+tala2
+print("tölurnar lagðar saman = "+str(plus)+" tölurnar margfaldaðar saman = "+str(marg))
+
